0931055772ddee136cd1211e33683e0fe537485d
WarWarKyaw <warwar67890@gmail.com>
2023-05-05 14:22:07 +0630

    conditional rendering(toggle button)

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const app = Vue.createApp({
     // template:'<h2> I am the template</h2>'
     data(){
         return{
+            showBooks:true,
             title:'The final Empire',
             author:'Bradon Sanderson',
             age:45
         }
     },
     methods: {
-        changeTitle(title){
-            // this.title="words of Randiance"
-            this.title=title
+        // changeTitle(title){
+        //     // this.title="words of Randiance"
+        //     this.title=title
+        // }
+        toggleShowBooks(){
+            this.showBooks=!this.showBooks
         }
+
     },
 })
 app.mount('#app')
