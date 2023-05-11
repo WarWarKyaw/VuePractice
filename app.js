5a1953e8cd0fc8d646276e8129ff53fc42bbc0cb
War War Kyaw <warwar67890@gmail.com>
2023-05-11 22:23:59 +0630

    data and attribute binding

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 const app = Vue.createApp({
-    // template:'<h2> I am the template</h2>'
-    data(){
-        return{
-            showBooks:true,
-            // title:'The final Empire',
-            // author:'Bradon Sanderson',
-            // age:45,
-            // x:0,
-            // y:0
-            books:[
-                {title:'The name of the wind', author:'patrick routhfuss'},
-                {title:'The name of the kings', author:'brandon sanderson'},
-                {title:'The final empire', author:'brandon sanderson'}
-            
-            
-            ]
-        }
-    },
-    methods: {
-        // changeTitle(title){
-        //     // this.title="words of Randiance"
-        //     this.title=title
-        // }
-        toggleShowBooks(){
-            this.showBooks=!this.showBooks
+  // template:'<h2> I am the template</h2>'
+  data() {
+    return {
+      url: "http://www.thenetninja.co.uk",
+      showBooks: true,
+      // title:'The final Empire',
+      // author:'Bradon Sanderson',
+      // age:45,
+      // x:0,
+      // y:0
+      books: [
+        {
+          title: "The name of the wind",
+          author: "patrick routhfuss",
+          img: "/assets/1.jpg",
         },
-        handleEvent(e,data){
-            console.log(e,e.type)
-            if(data){
-                console.log(data)
-            }
+        {
+          title: "The name of the kings",
+          author: "brandon sanderson",
+          img: "/assets/2.jpg",
         },
-        handleMousemove(e){
-            this.x=e.offsetX
-            this.y=e.offsetY
-
-        }
-
+        {
+          title: "The final empire",
+          author: "brandon sanderson",
+          img: "/assets/3.jpg",
+        },
+      ],
+    };
+  },
+  methods: {
+    // changeTitle(title){
+    //     // this.title="words of Randiance"
+    //     this.title=title
+    // }
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
-})
-app.mount('#app')
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
+});
+app.mount("#app");
